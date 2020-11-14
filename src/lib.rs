a0f50c15e0fef437456fd998e41292fda318dc16
Delyan Dobrev <delqn.dobrev@gmail.com>
2020-11-14 11:41:18 +0200

    Arc Mutex

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
-use std::cell::RefCell;
-use std::rc::Rc;
+use std::sync::{Arc, Mutex, MutexGuard, PoisonError};
 
-type Resolve<T> = dyn FnOnce(T);
-type Reject<E> = dyn FnOnce(E);
+type Resolve<T> = dyn FnOnce(Option<T>);
+type Reject<E> = dyn FnOnce(Option<E>);
 
-struct InternalPromise<T, E> {
+pub struct InternalPromise<T, E> {
     result: Option<Result<T, E>>,
 }
 
 pub struct Promise<T, E> {
-    internal: Rc<RefCell<InternalPromise<T, E>>>,
+    internal: Arc<Mutex<InternalPromise<T, E>>>,
 }
 
 
@@ -38,13 +37,27 @@ impl<T: 'static, E: 'static> Promise<T, E> {
     where
         F: FnOnce(Box<Resolve<T>>, Box<Reject<E>>)
     {
-        let internal = Rc::new(RefCell::new(InternalPromise::new()));
+        let internal = Arc::new(Mutex::new(InternalPromise::new()));
         let resolver = internal.clone();
         let rejecter = internal.clone();
 
         executor(
-            Box::new(move |value| resolver.borrow_mut().resolve(Some(value))),
-            Box::new(move |reason| rejecter.borrow_mut().reject(Some(reason))),
+            Box::new(move |value| {
+                match resolver.lock() {
+                    Ok(mut resolver) => {
+                        resolver.resolve(value);
+                    },
+                    _ => panic!("Cannot lock resolver"),
+                }
+            }),
+            Box::new(move |reason| {
+                match rejecter.lock() {
+                    Ok(mut rejecter) => {
+                        rejecter.reject(reason);
+                    },
+                    _ => panic!("Cannot lock resolver"),
+                }
+            }),
         );
 
         Self {
@@ -52,8 +65,8 @@ impl<T: 'static, E: 'static> Promise<T, E> {
         }
     }
 
-    pub fn is_done(&self) -> bool {
-        self.internal.borrow().is_done()
+    pub fn is_done(&self) -> Result<bool, PoisonError<MutexGuard<'_, InternalPromise<T, E>>>> {
+        self.internal.lock().map(|internal| internal.is_done())
     }
 }
 
@@ -71,12 +84,36 @@ mod test {
 
     #[test]
     fn resolves() {
-        let p = Promise::<u32, u32>::new(|resolve, _| {
-            resolve(1);
+        let p = Promise::<_, u32>::new(|resolve, _| {
+            resolve(Some(1));
         });
 
-        assert!(p.internal.borrow().result.is_some());
-        assert!(p.internal.borrow().result.unwrap().is_ok());
-        assert_eq!(p.internal.borrow().result.unwrap().unwrap(), 1);
+        assert!(p.internal.lock().unwrap().result.is_some());
+        assert!(p.internal.lock().unwrap().result.unwrap().is_ok());
+        assert_eq!(p.internal.lock().unwrap().result.unwrap().unwrap(), 1);
+    }
+
+    #[test]
+    fn rejects() {
+        let p = Promise::<u32, _>::new(|_, reject| {
+            reject(Some("some error"));
+        });
+
+        assert!(!p.internal.lock().unwrap().result.is_some());
+        assert!(p.internal.lock().unwrap().result.unwrap().is_err());
+        assert_eq!(p.internal.lock().unwrap().result.unwrap().err(), "some error");
+    }
+
+    #[test]
+    fn thread() {
+        let p = Promise::<u32, u32>::new(|resolve, _| {
+            std::thread::spawn(move || {
+                resolve(Some(1));
+            });
+        });
+
+        assert!(p.internal.lock().unwrap().result.is_some());
+        assert!(p.internal.lock().unwrap().result.unwrap().is_ok());
+        assert_eq!(p.internal.lock().unwrap().result.unwrap().unwrap(), 1);
     }
 }
