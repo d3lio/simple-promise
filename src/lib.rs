5b040e4a55703e9f1cbfb0b048a07660b110a3ab
Delyan Dobrev <delqn.dobrev@gmail.com>
2020-12-02 18:14:37 +0200

    Channels

--- src/lib.rs
+++ src/lib.rs
@@ -1,75 +1,31 @@
-use std::sync::{Arc, Mutex, MutexGuard, PoisonError};
+use std::sync::mpsc::{channel, Sender, Receiver};
 
-type Resolve<T> = dyn FnOnce(Option<T>) + Send;
-type Reject<E> = dyn FnOnce(Option<E>) + Send;
-
-pub struct InternalPromise<T, E> {
-    result: Option<Result<T, E>>,
+#[derive(Debug, Eq, PartialEq)]
+pub enum PromiseResult<T: Send> {
+    Pending,
+    Resolved(T),
+    Rejected(T),
 }
 
-pub struct Promise<T, E> {
-    internal: Arc<Mutex<InternalPromise<T, E>>>,
+pub struct Promise<T: Send> {
+    #[allow(dead_code)]
+    receiver: Receiver<PromiseResult<T>>,
 }
 
-
-impl<T: 'static + Send, E: 'static + Send> InternalPromise<T, E> {
-    fn new() -> Self {
-        Self {
-            result: None,
-        }
-    }
-
-    fn is_done(&self) -> bool {
-        self.result.is_some()
-    }
-
-    fn resolve(&mut self, value: Option<T>) {
-        self.result = value.map(Ok);
-    }
-
-    fn reject(&mut self, reason: Option<E>) {
-        self.result = reason.map(Err);
-    }
-}
-
-impl<T: 'static + Send, E: 'static + Send> Promise<T, E> {
+impl<T: Send> Promise<T> {
     pub fn new<F>(executor: F) -> Self
     where
-        F: FnOnce(Box<Resolve<T>>, Box<Reject<E>>)
+        F: FnOnce(Sender<PromiseResult<T>>)
     {
-        let internal = Arc::new(Mutex::new(InternalPromise::new()));
-        let resolver = internal.clone();
-        let rejecter = internal.clone();
+        let (sender, receiver) = channel();
 
-        executor(
-            Box::new(move |value| {
-                match resolver.lock() {
-                    Ok(mut resolver) => {
-                        resolver.resolve(value);
-                    },
-                    _ => panic!("Cannot lock resolver"),
-                }
-            }),
-            Box::new(move |reason| {
-                match rejecter.lock() {
-                    Ok(mut rejecter) => {
-                        rejecter.reject(reason);
-                    },
-                    _ => panic!("Cannot lock resolver"),
-                }
-            }),
-        );
+        executor(sender);
 
         Self {
-            internal,
+            receiver,
         }
     }
-
-    pub fn is_done(&self) -> Result<bool, PoisonError<MutexGuard<'_, InternalPromise<T, E>>>> {
-        self.internal.lock().map(|internal| internal.is_done())
-    }
 }
-
 
 #[cfg(test)]
 mod test {
@@ -78,46 +34,42 @@ mod test {
     #[test]
     fn executes() {
         let mut called = false;
-        let _ = Promise::<u32, u32>::new(|_, _| called = true);
+        let _ = Promise::<u32>::new(|_| called = true);
         assert!(called);
     }
 
     #[test]
     fn resolves() {
-        let p = Promise::<_, u32>::new(|resolve, _| {
-            resolve(Some(1));
+        let p = Promise::<u32>::new(|sender| {
+            let _ = sender.send(PromiseResult::Resolved(1));
         });
 
-        assert!(p.internal.lock().unwrap().result.is_some());
-        assert!(p.internal.lock().unwrap().result.unwrap().is_ok());
-        assert_eq!(p.internal.lock().unwrap().result.unwrap().unwrap(), 1);
+        assert_eq!(p.receiver.recv(), Ok(PromiseResult::Resolved(1)));
     }
 
     #[test]
     fn rejects() {
-        let p = Promise::<u32, _>::new(|_, reject| {
-            reject(Some("some error"));
+        let p = Promise::<u32>::new(|sender| {
+            let _ = sender.send(PromiseResult::Rejected(1));
         });
 
-        assert_eq!(p.internal.lock().unwrap().result.unwrap(), Err("some error"));
+
+        assert_eq!(p.receiver.recv(), Ok(PromiseResult::Rejected(1)));
     }
 
     #[test]
     fn thread() {
         let mut handle = None;
-        let p = Promise::<u32, u32>::new(|resolve, _| {
+        let p = Promise::<u32>::new(|sender| {
             handle = Some(std::thread::spawn(move || {
-                resolve(Some(1));
+                let _ = sender.send(PromiseResult::Rejected(1));
             }));
         });
 
-        // Flaky test
-        // std::thread::sleep(std::time::Duration::from_millis(1000));
-
-        assert_eq!(p.internal.lock().unwrap().result, None);
+        assert!(handle.is_some());
 
         handle.unwrap().join().unwrap();
 
-        assert_eq!(p.internal.lock().unwrap().result, Some(Ok(1)));
+        assert_eq!(p.receiver.recv(), Ok(PromiseResult::Rejected(1)));
     }
 }
